ef69836ff15966eda74526c22bbc56bbc3245434
MOSQUEDACORDOVA <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2022-06-20 15:13:29 -0700

    06202022

--- src/config/db.js
+++ src/config/db.js
@@ -1,36 +1,36 @@
 /** This script is for connect to SQL server with Tedius */
 var Connection = require('tedious').Connection;  
-// var db = {  
-// 	server: 'SQL1.riboli.local',  //SQL SERVER
-// 	authentication: {
-// 		type: 'default',
-// 		options: {
-// 			userName: 'sa', //USER
-// 			password: `Q"lS:MSZGd3=5UMZ`  //PASS
-// 		}
-// 	},
-// 	options: {
-// 		//Encrypt and get the database
-// 		encrypt: true,
-// 		database: 'X3Connect'  //Database Name
-// 	}
-// }; 
-
-var db = {
-	server: 'sql1.riboli.local', //SQL SERVER
+var db = {  
+	server: 'SQL1.riboli.local',  //SQL SERVER
 	authentication: {
-	type: 'default',
-	options: {
-	userName: 'portal', //USER
-	password: `GNnIRu!6!X3RItIq` //PASS
-	}
+		type: 'default',
+		options: {
+			userName: 'sa', //USER
+			password: `Q"lS:MSZGd3=5UMZ`  //PASS
+		}
 	},
 	options: {
-	//Encrypt and get the database
-	encrypt: true,
-	database: 'portal' //Database Name
+		//Encrypt and get the database
+		encrypt: true,
+		database: 'X3Connect'  //Database Name
 	}
-	};
+}; 
+
+// var db = {
+// 	server: 'SQL1.riboli.local', //SQL SERVER
+// 	authentication: {
+// 	type: 'default',
+// 	options: {
+// 	userName: 'portal', //USER
+// 	password: `GNnIRu!6!X3RItIq` //PASS
+// 	}
+// 	},
+// 	options: {
+// 	//Encrypt and get the database
+// 	encrypt: true,
+// 	database: 'portal' //Database Name
+// 	}
+// 	};
 module.exports = db;
 
 
--- src/config/dbSequelize.js
+++ src/config/dbSequelize.js
@@ -1,17 +1,17 @@
 /**This script is for connect to SQL server with Sequelize and use the models for manipulated the tables  */
 const { Sequelize } = require('sequelize');
 
-// DB_NAME="X3Connect";
-// DB_USER="sa";
-// DB_PASS=`Q"lS:MSZGd3=5UMZ`;
-// DB_HOST="SQL1.riboli.local";
-// DB_PORT=3306;
-
-DB_NAME="portal";
-DB_USER="portal";
-DB_PASS=`GNnIRu!6!X3RItIq`;
+DB_NAME="X3Connect";
+DB_USER="sa";
+DB_PASS=`Q"lS:MSZGd3=5UMZ`;
 DB_HOST="SQL1.riboli.local";
 DB_PORT=3306;
+
+// DB_NAME="portal";
+// DB_USER="portal";
+// DB_PASS=`GNnIRu!6!X3RItIq`;
+// DB_HOST="SQL1.riboli.local";
+// DB_PORT=3306;
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASS,
 	{
 		host: DB_HOST,
